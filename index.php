9bf5d70e0e48aa65af12dea53cbf50dec6876385
YARE <yarethhernandez20010207@gmail.com>
2024-04-29 00:00:14 -0600

    el primer proyecto

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+//En este apartado encontramos las funciones de localizacion del index
+if (!empty($_SESSION['active'])) {
+    header('location: src/');
+} else {
+    if (!empty($_POST)) {
+        $alert = '';
+//En este apartado estamos ucupando las variables de usuario y la clave de acceso para poder ingresar
+        if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+            $alert = '<div class="alert alert-danger" role="alert">
+            Ingrese su usuario y su clave
+            </div>';
+        } else {
+//En este aprtado tenemos la declaracion de codigo que tiene enlace con la "conexion" para que pueda tener funconamiento y aqui mismo tenemos el usuario y la clave de acceso
+            require_once "conexion.php";
+            $user = mysqli_real_escape_string($conexion, $_POST['usuario']);
+            $clave = mysqli_real_escape_string($conexion, $_POST['clave']);
+            echo("Hola".$clave);
+//En este apartado tenemos la conexion de bases de datos y las primeras tablas que creamos
+            $query = mysqli_query($conexion, "SELECT * FROM usuario WHERE usuario = '$user' AND clave = '$clave' AND estado = 1");
+            mysqli_close($conexion);
+            $resultado = mysqli_num_rows($query);
+            if ($resultado > 0) {
+                $dato = mysqli_fetch_array($query);
+                $_SESSION['active'] = true;
+                $_SESSION['idUser'] = $dato['id_usuario'];
+                $_SESSION['nombre'] = $dato['nombre'];
+                $_SESSION['user'] = $dato['usuario'];
+                $_SESSION['Fecha'] = $dato['Fecha'];
+//En este apartado tenemos el apartado de error  
+                header('location: src/');
+            } else {
+                $alert = '<div class="alert alert-danger" role="alert">
+                Usuario o Contraseña Incorrecta
+                </div>';
+                session_destroy();
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Iniciar Sessión</title>
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <script src="assets/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header text-center">
+                                    <img class="img-thumbnail" src="assets\img\astr.png" width="300">
+                                    <h3 class="font-weight-light my-4">Iniciar Sessión</h3>
+                                </div>
+                                <div class="card-body">
+                                    <form action="" method="POST">
+                                        <div class="form-group">
+                                            <label class="small mb-1" for="usuario"><i class="fas fa-user"></i> Usuario</label>
+                                            <input class="form-control py-4" id="usuario" name="usuario" type="text" placeholder="Ingrese usuario" required />
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="small mb-1" for="clave"><i class="fas fa-key"></i> Contraseña</label>
+                                            <input class="form-control py-4" id="clave" name="clave" type="password" placeholder="Ingrese Contraseña" required />
+                                        </div>
+                                        <div class="alert alert-danger text-center d-none" id="alerta" role="alert">
+
+                                        </div>
+                                        <?php echo isset($alert) ? $alert : ''; ?>
+                                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <button class="btn btn-primary" type="submit">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div id="layoutAuthentication_footer">
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; <a href="http://angelsifuentes.com/" target="_blank" rel="noopener noreferrer">Visite mi página web</a> <?php echo date("Y"); ?></div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="assets/js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+<a href="wwww.google.com">ir a google</a>
+
+</html>
